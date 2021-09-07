7a409737f904e885680255ade691b85b14eb3629
Luk3-S <lukesp63@gmail.com>
2021-09-07 11:31:00 +0100

    Reset project. Pure django for now, front end later

--- SLB_App/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SlbAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SLB_App'
--- SLB_App/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Recipe(models.Model):
-    title = models.CharField(max_length=70,blank=False,default='')
-    url = models.URLField()
-    steps = models.TextField()
-
