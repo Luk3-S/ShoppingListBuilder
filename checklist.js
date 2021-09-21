7fa3a0b4b55bcbb1665d4adb6b962acf002a5a36
Luk3-S <lukesp63@gmail.com>
2021-09-21 16:42:55 +0100

    Fixed index page formatting issue

--- checklist.js
+++
@@ -1,9 +0,0 @@
-function getIngredients() {
-    var selecteditems = [];
-
-    $('#checkboxes input:checked').each(function () {
-        //selecteditems.push($(ob).val());
-        selecteditems.push("dfsajobgsdjgsbjod");
-
-    })
-};
